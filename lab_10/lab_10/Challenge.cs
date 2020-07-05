63c310c4d54c2bc8e7f7a3c8fbd8f320037482ed
Popov Nikita <58226087+Inoskeee@users.noreply.github.com>
2020-07-05 14:37:42 +0500

    Add files via upload

---
+++ lab_10/lab_10/Challenge.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_10
+{
+
+    public abstract class Challenge
+    {
+        
+        private string name;    //Имя студента
+        private string subject; //Предмет испытания
+        private bool done;      //Выполнено или нет
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Subject
+        {
+            get { return subject; }
+            set { subject = value; }
+        }
+        public bool Done
+        {
+            get { return done; }
+            set { done = value; }
+        }
+
+        public Challenge(string name, string subj, bool done)
+        {
+            Name = name;
+            Subject = subj;
+            Done = done;
+        }
+        public Challenge(string name, string subj)
+        {
+            Name = name;
+            Subject = subj;
+        }
+        public Challenge()
+        {
+            name = "No Name";
+            subject = "No Subject";
+            done = false;
+        }
+
+        abstract public void Show();
+    }
+
+    public class Test : Challenge, IComparable, ICloneable
+    {
+        private int questCount;
+        private string theme;
+        private int result;
+
+
+        public int QuestCount
+        {
+            get { return questCount; }
+            set
+            {
+                if (value >= 0)
+                {
+                    questCount = value;
+                }
+                else
+                {
+                    questCount = 0;
+                }
+            }
+        }
+        public string Theme
+        {
+            get { return theme; }
+            set
+            {
+                theme = value;
+            }
+        }
+        public int Result
+        {
+            get { return result; }
+            set
+            {
+                result = value;
+            }
+        }
+
+        public Test(string name, string subj, string theme, int count, int result) : base(name, subj)
+        {
+            QuestCount = count;
+            Theme = theme;
+            if(result <= count)
+            {
+                Result = result;
+                if(result >= count / 2)
+                {
+                    Done = true;
+                }
+                else
+                {
+                    Done = false;
+                }
+            }
+            else
+            {
+                Result = count;
+                Done = true;
+            }
+        }
+        public Test():base()
+        {
+            questCount = 0;
+            theme = "No Theme";
+        }
+
+        public Test ShallowCopy() //Поверхностное копирование
+        {
+            return (Test)this.MemberwiseClone();
+        }
+        public object Clone() //Полное копирование
+        {
+            return new Test("Клон " + this.Name, this.Subject, this.Theme, this.QuestCount, this.Result);
+        }
+        public int CompareTo(object obj)
+        {
+            Test temp = (Test)obj;
+            if (String.Compare(this.Name, temp.Name) > 0) return 1;
+            if (String.Compare(this.Name, temp.Name) < 0) return -1;
+            return 0;
+        }
+
+        public override void Show()
+        {
+            Console.WriteLine("------------------");
+            Console.WriteLine($"Студент: {Name}");
+            Console.WriteLine($"Тест: {Subject}, \nТема: {Theme}, \nКоличество вопросов: {questCount}, \nВыполнено: {Done}");
+        }
+    }
+
+
+    public class Exam : Challenge, IComparable
+    {
+        private int questCount; //количество вопросов
+        private int result;     //результат экзамена
+        private bool essential; //обязательный или нет
+        private string marker;
+
+        public int QuestCount
+        {
+            get { return questCount; }
+            set
+            {
+                if (value >= 0)
+                {
+                    questCount = value;
+                }
+                else
+                {
+                    questCount = 0;
+                }
+            }
+        }
+        public int Result
+        {
+            get { return result; }
+            set
+            {
+                result = value;
+            }
+        }
+        public bool Essential
+        {
+            get
+            {
+                return essential;
+            }
+            set
+            {
+                essential = value;
+            }
+        }
+
+        public string Marker
+        {
+            get { return marker; }
+            set { marker = value; }
+        }
+
+        public Exam(string name, string subj, int count, int result, bool essential) :base(name, subj)
+        {
+            QuestCount = count;
+            Essential = essential;
+            if (result <= count)
+            {
+                Result = result;
+                if (result >= count / 2)
+                {
+                    if (result == count)
+                    {
+                        marker = "Отлично";
+                    }
+                    else if (result >= count / 4)
+                    {
+                        marker = "Хорошо";
+                    }
+                    else
+                    {
+                        marker = "Средне";
+                    }
+                    Done = true;
+                }
+                else
+                {
+                    Done = false;
+                    marker = "Не сдал";
+                }
+            }
+            else
+            {
+                Result = count;
+                Done = true;
+                marker = "Отлично";
+            }
+        }
+        public Exam(string name, string subj, int count, int result) : base(name, subj)
+        {
+            QuestCount = count;
+            if (result <= count)
+            {
+                Result = result;
+                if (result >= count / 2)
+                {
+                    if(result == count)
+                    {
+                        marker = "Отлично";
+                    }
+                    else if(result >= count / 4)
+                    {
+                        marker = "Хорошо";
+                    }
+                    else
+                    {
+                        marker = "Средне";
+                    }
+                    Done = true;
+                }
+                else
+                {
+                    Done = false;
+                    marker = "Не сдал";
+                }
+            }
+            else
+            {
+                Result = count;
+                Done = true;
+                marker = "Отлично";
+            }
+        }
+        public Exam() : base()
+        {
+            questCount = 0;
+            result = 0;
+            essential = false;
+        }
+
+
+        public int CompareTo(object obj)
+        {
+            Exam temp = (Exam)obj;
+            if (temp.Result < this.Result) return 1;
+            if (temp.Result > this.Result) return -1;
+            return 0;
+        }
+
+        public override void Show()
+        {
+            Console.WriteLine("------------------");
+            Console.WriteLine($"Студент: {Name}");
+            Console.WriteLine($"Тест: {Subject}, \nКоличество вопросов: {QuestCount}, \nРезультат: {Result}, \nОценка: {Marker}, \nЗачет: {Done}, \nОбязательный: {Essential}");
+        }
+
+    }
+
+    public class ExitExam : Exam, IComparable
+    {
+        private double influence; //Как сильно влияет экзамен на итоговую оценку
+        public double Influence
+        {
+            get { return influence; }
+            set
+            {
+                if(value <= 1 && value >= 0)
+                {
+                    influence = value;
+                }
+                else if(value > 1)
+                {
+                    influence = 1;
+                }
+                else
+                {
+                    influence = 0;
+                }
+            }
+        }
+
+        public ExitExam(string name, string subj, int count, int result, double marker):base(name, subj, count, result)
+        {
+            Influence = marker;
+        }
+        public ExitExam() : base()
+        {
+            Influence = 0.5;
+        }
+
+        public override void Show()
+        {
+            Console.WriteLine("------------------");
+            Console.WriteLine($"\nСтудент: {Name}");
+            Console.WriteLine($"Тест: {Subject}, \nКоличество вопросов: {QuestCount}, \nРезультат: {Result}, \nОценка: {Marker},  \nЗачет: {Done}, \nВлияние на итоговую оценку: {Influence}");
+        }
+
+    }
+
+    public class SortByName : IComparer<Test>
+    {
+        int IComparer<Test>.Compare(Test ob1, Test ob2)
+        {
+            return String.Compare(ob1.Name, ob2.Name);
+        }
+
+    }
+
+}
---
+++ lab_10/lab_10/Program.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_10
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Challenge[] array = new Challenge[4];
+
+            Test quest = new Test("Иван", "Математика", "Производные", 13, 18);
+            array[0] = quest;
+            Test test = new Test("Дмитрий", "Дискретная математика", "Графы", 25, 18);
+            array[1] = test;
+            Exam exam = new Exam("Алексей", "Программирование", 30, 1);
+            array[2] = exam;
+            ExitExam exExam = new ExitExam("Артем", "Физика", 25, 23, 1);
+            array[3] = exExam;
+
+            for (int i = 0; i<array.Length; i++)
+            {
+                array[i].Show();
+            }
+            Console.WriteLine("Введите название предмета:");
+            string subject = Console.ReadLine();
+            Console.WriteLine(Students(array, subject));
+            Console.WriteLine(Students_Count(array) + " cтудентов");
+            Console.WriteLine(Students_Fail(array) + " cтудентов");
+
+            Console.ReadKey();
+            Console.Clear();
+            //Сортировка объектов
+            Test_Sort();
+
+            Console.ReadKey();
+            Console.Clear();
+            //Клонирование объектов
+            Clone_Test();
+            Console.ReadKey();
+        }
+
+        //Имена студентов, сдавших все (заданный) экзамены на отлично (хорошо и отлично).
+        public static string Students(Challenge[] arr, string exam)
+        {
+            Console.WriteLine("-------------------");
+            Console.WriteLine("Имена студентов, сдавших экзамены по предмету "+exam+" на отлично");
+            for(int i = 0; i < arr.Length; i++)
+            {
+                Exam ex = arr[i] as Exam;
+                if (ex != null)
+                {
+                    if(ex.Subject == exam && (ex.Marker == "Хорошо" || ex.Marker == "Отлично"))
+                    {
+                        return ex.Name;
+                    }
+                }
+            }
+            return "";
+        }
+        public static string Students(Challenge[] arr)
+        {
+            Console.WriteLine("-------------------");
+            Console.WriteLine("Имена студентов, сдавших все экзамены на отлично");
+            string name = "";
+            List<string> names = new List<string>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Exam ex = arr[i] as Exam;
+                if (ex != null)
+                {
+                    if (ex.Marker == "Хорошо" || ex.Marker == "Отлично")
+                    {
+                        if (!names.Contains(ex.Name))
+                        {
+                            names.Add(ex.Name);
+                        }
+                    }
+                    else
+                    {
+                        if (!names.Contains(ex.Name))
+                        {
+                            names.Remove(ex.Name);
+                        }
+                    }
+                }
+            }
+            for(int k = 0; k<names.Count; k++)
+            {
+                name = names[k];
+            }
+            return name;
+        }
+
+        //Количество студентов, сдавших все экзамены на отлично.
+        public static int Students_Count(Challenge[] arr)
+        {
+            Console.WriteLine("-------------------");
+            Console.WriteLine("Количество студентов, сдавших все экзамены на отлично");
+            List<string> names = new List<string>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Exam ex = arr[i] as Exam;
+                if (ex != null)
+                {
+                    if (ex.Marker == "Хорошо" || ex.Marker == "Отлично")
+                    {
+                        if (!names.Contains(ex.Name))
+                        {
+                            names.Add(ex.Name);
+                        }
+                    }
+                    else
+                    {
+                        if (!names.Contains(ex.Name))
+                        {
+                            names.Remove(ex.Name);
+                        }
+                    }
+                }
+            }
+            return names.Count;
+        }
+
+        //Количество студентов, не сдавших хотя бы один экзамен.
+        public static int Students_Fail(Challenge[] arr)
+        {
+            Console.WriteLine("-------------------");
+            Console.WriteLine("Количество студентов, не сдавших хотя бы один экзамен");
+            List<string> names = new List<string>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Exam ex = arr[i] as Exam;
+                if (ex != null)
+                {
+                    if (ex.Done == false)
+                    {
+                        if (!names.Contains(ex.Name))
+                        {
+                            names.Add(ex.Name);
+                        }
+                    }
+                }
+            }
+            return names.Count;
+        }
+
+        //Различные сортировки объектов
+
+        static void Exam_Sort()
+        {
+            Console.WriteLine("Сортировка по результату теста.");
+
+            Exam[] array = new Exam[4];
+            array[0] = new Exam("Алексей", "Программирование", 30, 44);
+            array[1] = new Exam("Дмитрий", "Программирование", 30, 7);
+            array[2] = new Exam("Иван", "Программирование", 30, 1);
+            array[3] = new Exam("Сергей", "Программирование", 30, 4);
+
+            Array.Sort(array);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i].Show();
+            }
+        }
+        static void Test_Sort()
+        {
+            Console.WriteLine("Сортировка по имени");
+
+            Test[] array = new Test[4];
+            array[0] = new Test("Алексей", "Математика", "Производные", 13, 18);
+            array[1] = new Test("Антон", "Математика", "Производные", 13, 18);
+            array[2] = new Test("Михаил", "Математика", "Производные", 13, 18);
+            array[3] = new Test("Василий", "Математика", "Производные", 13, 18);
+
+            Array.Sort(array, new SortByName());
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i].Show();
+            }
+        }
+
+        //Клонирование объектов
+
+        static void Clone_Test()
+        {
+            Console.WriteLine("Клонирование объектов");
+
+            Test[] array = new Test[4];
+            array[0] = new Test("Алексей", "Математика", "Производные", 15, 10);
+            array[1] = new Test("Антон", "История", "19 век", 13, 11);
+            array[2] = new Test("Михаил", "Программирование", "Классы", 34, 28);
+            array[3] = new Test("Василий", "Дискретная математика", "Графы", 5, 3);
+            foreach(Test obj in array)
+            {
+                obj.Show();
+            }
+            Console.WriteLine("Объекты клоны: ");
+            Test clone_1 = array[1].ShallowCopy();
+            clone_1.Show();
+            Test clone_2 = (Test)array[1].Clone();
+            clone_2.Show();
+        }
+    }
+}
